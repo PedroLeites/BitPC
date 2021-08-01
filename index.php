6b76adf14f1eed58f8c4d773e2ab4e0e7b5295cd
Pedro Leites <leitespsinsajo@gmail.com>
2021-08-01 15:33:01 -0300

    Login Completo

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-require 'libs/database.php';
-require 'libs/model.php';
-require 'libs/controller.php';
-require 'libs/view.php';
-require 'libs/app.php';
+require_once 'libs/database.php';
+require_once 'libs/model.php';
+require_once 'libs/controller.php';
+require_once 'libs/view.php';
+require_once 'libs/app.php';
 
 require 'config/config.php';
 $app = new App();
