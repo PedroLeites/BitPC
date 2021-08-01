66984878439fa98e6a8682d5a31989d29cc112ed
Pedro Leites <leitespsinsajo@gmail.com>
2021-08-01 15:20:14 -0300

    correccion de errores de base de datos

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/proyectofinal3bj/BitPC/');
 //conexion a la base de datos
 define('HOST', 'localhost');
 define('PORT', '3306');
-define('DB', 'basebit');
-define('USER', 'adminbit');
-define('PASSWORD', "adminbit");
-define('CHARSET', 'utf8mb4_unicode_ci');
+define('DB', 'basebitV02');
+define('USER', 'usuario03');
+define('PASSWORD', 'usuario03');
+define('CHARSET', 'utf8mb4');
