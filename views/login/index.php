e3776ea6d4927651e8730a9c712c9b79892e7c90
Pedro Leites <leitespsinsajo@gmail.com>
2021-08-01 12:47:26 -0300

    agregado login vista

---
+++ views/login/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Ingresar</title>
+</head>
+<body>
+<div>
+    <div>
+        <div>
+            <h1>Login</h1>
+        </div>
+    </div>
+    <div>
+        <div>
+            <h1><?php echo $this->resultadoLogin; ?></h1>
+        </div>
+    </div>
+        <form action="<?php echo constant('URL'); ?>login/ingresar" method="post">
+        <div>
+            <label for="username">Nombre de usuario</label>
+            <input type="text" id="username" name="nombre">
+        </div>
+        <div>
+            <label for="password">Contraseña</label>
+            <input type="password" class="form-control" id="password" name="pass">
+        </div>
+        <div>
+            <button type="submit">Ingresar</button>
+        </div>
+        </form>
+    </div>
+</body>
+</html>
