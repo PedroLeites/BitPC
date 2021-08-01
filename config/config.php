d869478d16eed3bee653571ab2227a9139dff1b8
Pedro Leites <leitespsinsajo@gmail.com>
2021-08-01 12:23:52 -0300

    base de datos provisional creada y vinculada

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-//define('URL', 'http://localhost/mvc/');
+//define url
 define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/proyectofinal3bj/BitPC/');
 
 //conexion a la base de datos
 define('HOST', 'localhost');
 define('PORT', '3306');
-define('DB', 'base03');
-define('USER', 'usuario03');
-define('PASSWORD', "usuario03");
-define('CHARSET', 'utf8mb4');
+define('DB', 'baseBit');
+define('USER', 'adminbit');
+define('PASSWORD', "adminbit");
+define('CHARSET', 'utf8mb4_unicode_ci');
