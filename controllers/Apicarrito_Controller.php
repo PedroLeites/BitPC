786d3b4fa5c856752310dec2967d997a5369002b
pedroaxelfranco <leitespsinsajo@gmail.com>
2021-09-14 16:32:46 -0300

    Agregado de Apicarrito_Controller

--- controllers/Apicarrito_Controller.php
+++ controllers/Apicarrito_Controller.php
@@ -1,2 +1,55 @@
 <?php
 require_once 'entidades/carrito.php';
+class Apicarrito_Controller extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    //localhost/proyectofinal3bj/BitPC/Apicarrito/completarCarrito
+    public function completarCarrito()
+    {
+        $json = file_get_contents('php://input');
+        //convierto el json en un array asociativo de php
+        $datos = json_decode($json);
+        $listaArticulos = $datos->lista;
+        //$usuario = $datos->usuario;
+        //lista = array();
+        $lista = [];
+        foreach ($listaArticulos as $key => $obj) {
+            $articulo = new Carrito();
+            $articulo->id = $obj->id;
+            $articulo->cantidad = $obj->cantidad;
+            $articulo->precio = $obj->precio;
+            $lista[] = $articulo;
+            //array_push($lista, $articulo);
+        }
+        $resultado = $this->model->completarCarrito($lista, $usuario);
+
+        $respuesta = [
+            "datos" => $lista,
+            "totalResultados" => count($lista),
+            "usuario" => $usuario,
+            "resultado" => $resultado,
+        ];
+        $this->view->respuesta = json_encode($respuesta);
+        if ($resultado == false) {
+            http_response_code(400);
+            $this->view->respuesta = json_encode([
+                "resultado" => $resultado,
+                "respuesta" => "error al completar el pedido",
+            ]);
+        } else {
+            http_response_code(200);
+        }
+        $this->view->render('api/carrito/completarcarrito');
+        //var_dump($this);
+        //var_dump($this->view);
+        //$this->view->render('apilea/articulos/index');
+        //var_dump($this);
+        //var_dump($this->view);
+    }
+
+}
