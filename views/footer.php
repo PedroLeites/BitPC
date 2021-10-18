a944205fa1d03c6ff754f41bc49d4c8f433cd474
Pedro Leites <leitespsinsajo@gmail.com>
2021-10-18 18:59:23 -0300

    Creando Footer

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,44 @@
+<link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/footer/footer.css">
+<footer>
+<div class="container_footer">
+  <div class="row">
+    <div class="logo_pc"">
+      <img src="<?php echo constant('URL'); ?>public/img/logos/logo-footer.png" alt="">
+    </div>
+    <div class="desc">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique velit nobis possimus quidem quos. Perspiciatis quisquam ad sit sequi sapiente, odio voluptate, nam possimus ipsum itaque quasi ea tenetur dolores.</p>
+    </div>
+    <div class="redes">
+      <ul>
+        <li>Face</li>
+        <li>Instagram</li>
+        <li>TikTok</li>
+      </ul>
+    </div>
+  </div>
+  <div class="row">
+    <h2>CONTÁCTENOS</h2>
+    <ul>
+      <li>Av. Artigas, 15900 Las Piedras, Departamento de Canelones</li>
+      <li>23691032</li>
+      <li>093343923</li>
+      <li>bitpc@gmail.com</li>
+    </ul>
+  </div>
+  <div class="row">
+    <div class="mapa"></div>
+  </div>
+  <div class="row">
+    <div class="code-logo">
+      <img src="<?php echo constant('URL'); ?>public/img/logos/code-footer.png" alt="">
+    </div>
+    <div class="desc">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, omnis inventore ullam consequuntur libero possimus totam modi? Placeat architecto, assumenda inventore ab ex reiciendis, praesentium, debitis atque maiores hic exercitationem.
+    </div>
+  </div>
+</div>
+</footer>
+
 <script src="<?php echo constant('URL'); ?>/public/js/jquery-3.6.0.min.js"></script>
 <script src="<?php echo constant('URL'); ?>public/js/main/main.js"></script>
 <script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
