855e5f313c2e9d433ddb45071a45ed92fa45127b
Pedro Leites <leitespsinsajo@gmail.com>
2021-11-14 18:58:19 -0300

    Agregando estilos a forms Login, Registro e Idioma

--- views/idioma/index.php
+++ views/idioma/index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/idioma/idioma.css">
   <title>BIT PC - Idioma</title>
 </head>
 
@@ -12,12 +13,11 @@
   <?php require_once 'views/header.php';?>
   <?php require_once 'views/buscador.php';?>
   <input type="hidden" value="<?php echo constant('URL'); ?>" id="url">
-  <div>
-    <h1>Selección de Idioma</h1>
-  </div>
-  <div>
-    <form action="<?php echo constant('URL'); ?>idioma/cambiarIdioma" method="post">
+  <div id="form-container">
+    <form id="form-idioma" action="<?php echo constant('URL'); ?>idioma/cambiarIdioma" method="post">
+      <h1>Seleccionar Idioma</h1>
       <select name="idioma" id="cambioIdioma">
+        <option disabled selected>Selecciona una opción</option>
         <option>es</option>
         <option>en</option>
       </select>
